934e484ea113d9058c4c2b0b66795e30cd255b53
Vinicius Dias <diasvinicius95@outlook.com>
2023-08-12 14:36:03 -0300

    alterando a description

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ const inter = Poppins({ subsets: ['latin'], weight:['300','400','500','600'] })
 
 export const metadata: Metadata = {
   title: 'Vinícius Dias',
-  description: 'Vinícius Dias is a software enginier and frelancer at Brazil. Im aways trying new technologies and open to learn.',
+  description: 'Vinícius Dias is a software developer and frelancer at Brazil. Im aways trying new technologies and open to learn.',
 }
 
 export default function RootLayout({
